aee2b612adddcc6f6efe779665c35c5131425014
Atlantic Press <40704323+labrador936@users.noreply.github.com>
2020-05-01 20:48:49 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const queue = new Map();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`By Floki /play |https://atlantic95a.com`)
+client.user.setGame(`|https://atlantic95a.com`)
   console.log('')
   console.log('')
   console.log('+[-----------------------------------------------------------------]+')
